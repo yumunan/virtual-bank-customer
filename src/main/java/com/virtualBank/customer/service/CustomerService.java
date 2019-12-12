d897230fedaad089c4b122e42ce2b261be8cc497
yumunan <apedweb#123>
2019-12-12 23:43:38 +0800

    restructure

--- src/main/java/com/virtualBank/customer/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.virtualBank.customer.service;
-
-import com.virtualBank.customer.domain.Customer;
-import com.virtualBank.customer.vo.Response;
-
-public interface CustomerService {
-    public Response login(Customer customer) ;
-}
